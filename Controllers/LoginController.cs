2d4e18528e94bba377ffd69fb83301a26bdf22b4
Sunil-kumar11 <jaysanny@gmail.com>
2021-11-24 18:41:02 +0530

    CreateStudent

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Login.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,14 +14,27 @@ namespace Login.Controllers
         {
             return View();
         }
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
         public List<Student> PutValues()
         {
-            var Students =new List<Student> { 
+            
+        
+            
+            List<Student> Students =new List<Student> { 
             new Student
             {
                 Id=121,
-                UserName = "raju",
+                UserName = "Raju",
                 Password = "Raju@123"
+            },
+            new Student
+            {
+                Id=124,
+                UserName = "raju",
+                Password = "Raju@456"
             },
             new Student
             {
@@ -41,25 +55,56 @@ namespace Login.Controllers
                 Password = "mahesh@123"
             }
           };
+
             return Students;
         }
 
+        [HttpPost]
         public IActionResult LoginUser(Student student)
         {
-            var Students = PutValues();
-            var name = Students.Where( x => x.UserName.Equals(student.UserName));
-            var pswd = Students.Where( x => x.Password.Equals(student.Password));
 
-            if(name.Count()==1 && pswd.Count() == 1)
+            string s = student.UserName;
+            string pswd = student.Password;
+            var Students = PutValues();
+            for (int i = 0; i < Students.Count - 1; i++)
             {
-                ViewBag.message = "Login Success";
-                return View("Login");
+                if (s == Students[i].UserName && pswd == Students[i].Password && Students[i].Id == Students[i].Id)
+                {
+                    ViewBag.message = "Login Success";
+                    return View("Login");
+                }
             }
-            else
-            {
-                ViewBag.message = "UserName Or Password is Wrong please enter correct UserName And Password";
+                ViewBag.message1 = "UserName Or Password is Wrong please enter correct UserName And Password";
                 return View("Login");
-            }
+
         }
+
+        
+
+
+        //var name = Students.Where(x => x.UserName.Equals(student.UserName));
+        //var pswd = name.Where(x => x.Password.Equals(student.Password));
+
+
+        //if (name.Count()>= 1)
+        //{
+        //    ViewBag.message = "Login Success";
+        //    return View("Login");
+        //}
+        //else
+        //{
+        //    ViewBag.message1 = "UserName Or Password is Wrong please enter correct UserName And Password";
+        //    return View("Login");
+        //}
+
+
+        [HttpPost]
+        public IActionResult PostStudents(Student student)
+        {
+
+            PutValues().Add(student);
+            ViewBag.message = "student details added successfully";
+            return View("Create");
+                   }
     }
 }
